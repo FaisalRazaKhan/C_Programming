9637dc38354e0bc18dfe56adb99e6e12bda2992d
Faisal Raza Khan <40921468+FaisalRazaKhan@users.noreply.github.com>
2023-12-15 16:08:35 +0530

    Create helloWorld.c

---
+++ helloWorld.c
@@ -0,0 +1,8 @@
+//this is a comment
+#include <stdio.h>    //including header file in our program
+
+int main()            //main() where the execution begins
+{
+    printf("Hello World");
+    return 0;
+}
