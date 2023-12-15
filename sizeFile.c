c52db64f96b13c94e82904046422aeafc31279ed
Faisal Raza Khan <40921468+FaisalRazaKhan@users.noreply.github.com>
2023-12-15 16:12:39 +0530

    Create sizeFile.c

---
+++ sizeFile.c
@@ -0,0 +1,25 @@
+Q ) C Program to find the Size of any File.
+
+  #include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    FILE *fp;
+    char ch;
+    int size = 0;
+
+    fp = fopen("MyFile.txt", "r");
+    if (fp == NULL)
+    {
+        printf("\nFile unable to open...");
+    }
+    else
+    {
+        printf("\nFile opened...");
+    }
+    fseek(fp, 0, 2);    /* File pointer at the end of file */
+    size = ftell(fp);   /* Take a position of file pointer in size variable */
+    printf("The size of given file is: %d\n", size);
+    fclose(fp);
+}
